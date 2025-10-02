5003028115ef7d37fe7ac112d2d3dabb84d6d039
Lourdes Nunes <lourdes.nu98@gmail.com>
2025-10-02 19:17:10 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let usuario = prompt("Ingrese su usuario")
+let contra = prompt("Ingrese su contraseña")
+
+if(usuario != "" )
+{
+    console.log ("Ingreso al sistema!. Bienvenido " + usuario)
+} 
+else 
+{
+console.log("No ingreso")
+}
